26804e1653f04de4bd27ffb0c4591f02a8e886e9
azinmapar <79557566+azinmapar@users.noreply.github.com>
2023-06-26 11:13:00 +0330

    done

--- lib/bindings/my_bindings.dart
+++ lib/bindings/my_bindings.dart
@@ -1,9 +1,11 @@
 import 'package:get/get.dart';
 import 'package:getx_shimmer_screen/controllers/navigation_bar_controller.dart';
+import 'package:getx_shimmer_screen/controllers/shimmer_contoller.dart';
 
 class MyBindings implements Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => NavigationBarController());
+    Get.lazyPut(() => ShimmerController());
   }
 }
--- lib/shimmer_screen.dart
+++ lib/shimmer_screen.dart
@@ -1,7 +1,7 @@
 import 'package:fade_shimmer/fade_shimmer.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
+import 'package:getx_shimmer_screen/controllers/shimmer_contoller.dart';
 
 class ShimmerScreen extends StatelessWidget {
   const ShimmerScreen({super.key});
@@ -10,7 +10,9 @@ class ShimmerScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Get.find<ShimmerController>().toggle();
+        },
         backgroundColor: Colors.grey[800],
         child: const Icon(
           Icons.toggle_off,
@@ -23,46 +25,60 @@ class ShimmerScreen extends StatelessWidget {
         child: ListView.separated(
           itemBuilder: (context, index) {
             var delay = (300 * index);
-            return Container(
-              padding: const EdgeInsets.all(10.0),
-              decoration: BoxDecoration(
-                color: const Color(0xFF242424),
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              child: Row(
-                children: [
-                  FadeShimmer.round(
-                    size: 60,
-                    fadeTheme: FadeTheme.dark,
-                    millisecondsDelay: delay,
-                  ),
-                  const SizedBox(
-                    width: 8.0,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      FadeShimmer(
-                        width: 100.0,
-                        height: 8.0,
-                        millisecondsDelay: delay,
-                        fadeTheme: FadeTheme.dark,
-                        radius: 4.0,
+            return Obx(
+              () => Get.find<ShimmerController>().isLoading.value
+                  ? Container(
+                      padding: const EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF242424),
+                        borderRadius: BorderRadius.circular(8.0),
                       ),
-                      const SizedBox(
-                        height: 8.0,
+                      child: Row(
+                        children: [
+                          FadeShimmer.round(
+                            size: 60,
+                            fadeTheme: FadeTheme.dark,
+                            millisecondsDelay: delay,
+                          ),
+                          const SizedBox(
+                            width: 8.0,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              FadeShimmer(
+                                width: 100.0,
+                                height: 8.0,
+                                millisecondsDelay: delay,
+                                fadeTheme: FadeTheme.dark,
+                                radius: 4.0,
+                              ),
+                              const SizedBox(
+                                height: 8.0,
+                              ),
+                              FadeShimmer(
+                                width: 170.0,
+                                height: 8,
+                                fadeTheme: FadeTheme.dark,
+                                millisecondsDelay: delay,
+                                radius: 4.0,
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
-                      FadeShimmer(
-                        width: 170.0,
-                        height: 8,
-                        fadeTheme: FadeTheme.dark,
-                        millisecondsDelay: delay,
-                        radius: 4.0,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                    )
+                  : Row(
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Colors.grey[800],
+                        ),
+                        const SizedBox(
+                          width: 8.0,
+                        ),
+                        const Text('Hello'),
+                      ],
+                    ),
             );
           },
           separatorBuilder: (context, index) {
